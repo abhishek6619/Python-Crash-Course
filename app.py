ba796c307272b30022247c36d29cd2f74c690cb8
Abhishek Kumar Singh <abhis42124@gmail.com>
2025-01-12 17:04:14 +0530

    Update the Code

--- app.py
+++ app.py
@@ -236,3 +236,14 @@ while command.lower() != "quit":
         print("Car started...")
     elif command.lower() == "stop":
         print("Car stopped.")
+    elif command == "help":
+        print("""
+        start - to start the car
+        stop - to stop the car
+        quit - to quit
+        """)
+    elif command == "quit":
+        break
+    else:
+        print("Sorry, I don`t understand that!")
+    
