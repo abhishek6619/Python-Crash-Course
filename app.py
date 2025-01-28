9eb35a471eeaa0aa78e3591f701e2dfe58bd3982
Abhishek Kumar Singh <abhis42124@gmail.com>
2025-01-28 08:48:39 +0530

    Update the Code

--- app.py
+++ app.py
@@ -234,7 +234,107 @@
 #     total += price
 # print(f"Total: {total}")
 
-# ------- nested loop ------- #
+# for x in "Python":
+#     print(x)
 
-for x in range(4):
-    print(x)
+# for x in ["a", "b", "c"]:
+#     print(x)
+
+# for x in range(0, 10, 2):
+#     print(x)
+
+# print(type(range(5)))
+# print([1, 2, 3, 4, 5])
+
+# names = ["John", "Mary"]
+# for name in names:
+#     if name.startswith("J"):
+#         print("Found")
+#         break
+# else:
+#     print("Not found")
+
+# ------- while loop ------- #
+
+# guess = 0
+# answer = 5
+
+# while answer != guess:
+#     guess = int(input("Guess: "))
+# else:
+#     pass
+
+# ------------------------ Function ------------------------ #
+
+
+# def increment(number: int, by: int = 1) -> tuple:
+#     return (number, number + by)
+
+
+# print(increment(2, by=3))
+# print(increment(2))
+
+# ------- args ------- #
+
+
+# def multiply(*list):
+#     total = 1
+#     for number in list:
+#         total *= number
+#     return total
+
+
+# print(multiply(2, 3, 4, 5))
+
+
+# def save_user(**user):
+#     print(user["name"])
+
+
+# save_user(id=1, name="admin")
+
+# ------- Scope ------- #
+
+# message = "a"
+
+
+# def greet():
+#     global message
+#     message = "b"
+#     # print(message)
+
+
+# greet()
+# print(message)
+
+# ------- Debugging ------- #
+
+
+# def multiply(*numbers):
+#     total = 1
+#     for number in numbers:
+#         total *= number
+#     return total
+
+
+# print("start")
+# print(multiply(1, 2, 3))
+# print("finish")
+
+
+# Q: fizz_buzz Problem
+
+
+def fizz_buzz(input):
+    if (input % 3 == 0) and (input % 5 == 0):
+        return "Fizz Buzz"
+    if input % 3 == 0:
+        return "Fizz"
+    if input % 5 == 0:
+        return "Buzz"
+    return input
+
+
+print(fizz_buzz(15))
+print(fizz_buzz(3))
+print(fizz_buzz(5))
